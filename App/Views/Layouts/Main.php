31b3d172f5183c75ecaa4ba08a846a75afd3c689
Kobus <kobus.haarhuis@hotmail.com>
2018-03-07 22:57:10 +0100

    css basics

--- App/Views/Layouts/Main.php
+++ App/Views/Layouts/Main.php
@@ -4,13 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Builder</title>
+    <link rel="stylesheet" href="min/main.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 </head>
 <body>
-    <?= component('header.php') ?>
-    <main>
-        {{ content }}
-    </main>
-    <?= component('footer.php') ?>
+
+    <div class="grid">
+
+        <div class="is-3" style="height: 20px; background: green;"></div>
+        <div class="is-2" style="height: 20px; background: red;"></div>
+        <div class="is-4" style="height: 20px; background: blue;"></div>
+
+    </div>
+
+    <div class="wrapper">
+
+        <header role="header" class="header">
+            <?= component('header.php') ?>
+        </header>
+
+        <main role="main" class="page">
+            {{ content }}
+        </main>
+
+        <footer role="footer" class="footer">
+            <?= component('footer.php') ?>
+        </footer>
+
+    </div>
+
+    <div role="scripts" class="hide">
+        
+        <script src="min/main.js"></script>
+    </div>
 </body>
 </html>
